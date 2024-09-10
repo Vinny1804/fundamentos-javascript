20199c50b2ef95675c99b898f21ce1b11e0666b9
Vinicius Bernardino Aguiar <vinny_aguiar@hotmail.com>
2024-09-10 09:25:42 -0300

    Add files via upload

---
+++ Avançando com ferramentas e paradigmas/Criando sua primeira biblioteca/src/erros/funcoesErro.js
@@ -0,0 +1,7 @@
+export default function tratarErros(erro) {
+    if (erro.code === 'ENOENT') {
+        throw new Error('Arquivo não encontrado');
+    } else {
+        return 'Erro na aplicação'
+    }
+}
